0a880f14bcf6cee32f7a87e3b1344f611929207d
aime1 <charles.at.nctu@gmail.com>
2016-09-01 16:38:40 -0700

    testing

---
+++ moveFileByName.sh
@@ -0,0 +1,5 @@
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+
+sleep 30
+
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
