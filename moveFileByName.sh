7156cefd833fe4f8d8979dbf4861442e139317dd
aime1 <charles.at.nctu@gmail.com>
2016-09-01 16:52:59 -0700

    testing

--- moveFileByName.sh
+++ moveFileByName.sh
@@ -1,5 +1,14 @@
 python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-
-sleep 30
-
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+sleep 10
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+sleep 10
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+sleep 10
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+sleep 10
+python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
 python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
