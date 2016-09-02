eb2212375e75261b9c1e71cb53884364afbb15a9
aime1 <charles.at.nctu@gmail.com>
2016-09-01 22:10:35 -0700

    testing

--- facerecognizer.py
+++ facerecognizer.py
@@ -2,9 +2,9 @@ import cv2, os
 import numpy as np
 from PIL import Image
 
-print cv2.__version__
+print cv2.__version__;
 
-recognizer = cv2.face.createLBPHFaceRecognizer()
+recognizer = cv2.face.createLBPHFaceRecognizer();
 recognizer = cv2.face.createLBPHFaceRecognizer();
 recognizer = cv2.face.createEigenFaceRecognizer();
 recognizer = cv2.face.createFisherFaceRecognizer();
