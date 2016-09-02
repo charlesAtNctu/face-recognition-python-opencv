1c2d09b13f1bbe416cbae2ba540f6d5150128c40
aime1 <charles.at.nctu@gmail.com>
2016-09-01 17:30:58 -0700

    testing

--- moveFileByName.sh
+++ moveFileByName.sh
@@ -1,14 +1,14 @@
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
 sleep 10
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
 sleep 10
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
 sleep 10
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
 sleep 10
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
