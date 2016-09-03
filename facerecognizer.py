48e0bd64d381ae6a7bf0e945fcfcbb18242a1ccb
aime1 <charles.at.nctu@gmail.com>
2016-09-02 21:07:16 -0700

    testing

--- facerecognizer.py
+++ facerecognizer.py
@@ -59,6 +59,18 @@ def main(argv):
     # Perform the tranining
     recognizer.train(images, np.array(labels))
 
+    image_path = "/var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/cliu/face_cliu_20160901174419978.png"
+    predict_image_pil = Image.open(image_path).convert('L')
+    predict_image = np.array(predict_image_pil, 'uint8')
+    faces = faceCascade.detectMultiScale(predict_image)
+    for (x, y, w, h) in faces:
+        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+        nbr_actual = 4;#int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+        if nbr_actual == nbr_predicted:
+            print "{} is Correctly Recognized with confidence {}".format(nbr_actual, conf)
+        else:
+            print "{} is Incorrect Recognized as {}".format(nbr_actual, nbr_predicted)
+
 if __name__ == "__main__":
     main(sys.argv)
 
