68687d65d5d8c8e3bde188c1a09965771424baf5
aime1 <charles.at.nctu@gmail.com>
2016-09-02 19:31:40 -0700

    testing

--- facerecognizer.py
+++ facerecognizer.py
@@ -14,18 +14,21 @@ faceCascade = cv2.CascadeClassifier(cascadePath)
 # For face recognition we will the the LBPH Face Recognizer
 recognizer = cv2.face.createLBPHFaceRecognizer();
 def get_images_and_labels(path):
-    # Append all the absolute image paths in a list image_paths
-    # We will not read the image with the .sad extension in the training set
-    # Rather, we will use them to test our accuracy of the training
+
+
+
     image_paths = [os.path.join(path, f) for f in os.listdir(path)]
+    print image_paths;# user dir abs path here !!!
+    images = []
 
-    print image_paths;
+    labels = []
+    for image_path in image_paths:
+        label = 0;
+        print image_path + " has label, " + str(label);
+        user_images = os.listdir(image_path)
+        for user_image in user_images:
+            print user_image;
 
-    # # images will contains face images
-    # images = []
-    # # labels will contains the label that is assigned to the image
-    # labels = []
-    # for image_path in image_paths:
     #     # Read the image and convert to grayscale
     #     image_pil = Image.open(image_path).convert('L')
     #     # Convert the image format into numpy array
@@ -52,8 +55,7 @@ def main(argv):
 if __name__ == "__main__":
     main(sys.argv)
 
-
-
+# python facerecognizer.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
 
 
 
