d2162445e415ae0d9e3f0e4084617d4d04f692cf
aime1 <charles.at.nctu@gmail.com>
2016-09-02 19:43:43 -0700

    testing

--- facerecognizer.py
+++ facerecognizer.py
@@ -28,10 +28,11 @@ def get_images_and_labels(path):
 
         print image_path + " has label, " + str(label);
         user_images = os.listdir(image_path)
-        for user_image_path in user_images:
+        for user_image in user_images:
+            user_image_path = image_path + "/" + user_image;
             print "USER IMAGE PATH: " + user_image_path;
             user_image_pil = Image.open(user_image_path).convert('L') # Read the image and convert to grayscale
-            #user_image = np.array(user_image_pil, 'uint8') # Convert the image format into numpy array
+            user_image_np = np.array(user_image_pil, 'uint8') # Convert the image format into numpy array
 
         label = label + 1;
 
