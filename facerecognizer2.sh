cfd2cd410aa7d00fc27924725815bd0956e56756
aime1 <charles.at.nctu@gmail.com>
2016-09-05 22:20:39 -0700

    make it executable

--- facerecognizer2.sh
+++ facerecognizer2.sh
@@ -1,9 +1,8 @@
-ERVICE='facerecognizer2.py'
- 
-if ps ax | grep -v grep | grep $SERVICE > /dev/null
+if ps ax | grep -v grep | grep facerecognizer2 > /dev/null
 then
     echo "$SERVICE service running, everything is fine"
 else
     echo "$SERVICE is not running and going to run now"
-    cd ~/GitHub/face-recognition-python-opencv/ ; python ~/GitHub/face-recognition-python-opencv/facerecognizer2.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/ /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+    cd /home/ubuntu/GitHub/face-recognition-python-opencv/
+    python /home/ubuntu/GitHub/face-recognition-python-opencv/facerecognizer2.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/ /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
 fi
