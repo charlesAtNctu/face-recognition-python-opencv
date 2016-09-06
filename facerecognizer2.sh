dc955862169c8f3c4c4644bda5ab633e0865c2de
aime1 <charles.at.nctu@gmail.com>
2016-09-05 22:35:00 -0700

    testing

--- facerecognizer2.sh
+++ facerecognizer2.sh
@@ -6,5 +6,6 @@ then
     echo "$SERVICE service running, everything is fine"
 else
     echo "$SERVICE is not running and going to run now"
+    cd /home/ubuntu/GitHub/face-recognition-python-opencv/
     python /home/ubuntu/GitHub/face-recognition-python-opencv/facerecognizer2.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/ /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
 fi
