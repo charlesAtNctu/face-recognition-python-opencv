53bc24b4ee93f52c7af37cf7d31c7993bcf492cb
aime1 <charles.at.nctu@gmail.com>
2016-09-05 22:30:00 -0700

    testing

--- facerecognizer2.sh
+++ facerecognizer2.sh
@@ -1,14 +1,10 @@
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
-sleep 10
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
-sleep 10
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
-sleep 10
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
-sleep 10
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ face_ 0 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
-python /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/      face_ 1 /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/
+#!/bin/sh
+SERVICE='facerecognizer2.py'
+ 
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo "$SERVICE service running, everything is fine"
+else
+    echo "$SERVICE is not running and going to run now"
+    python /home/ubuntu/GitHub/face-recognition-python-opencv/facerecognizer2.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/ /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
+fi
