bda3d8da6635ed7ff32b91dcde66b3b493e55f82
aime1 <charles.at.nctu@gmail.com>
2016-09-06 21:07:32 -0700

    next one check folder last modified

--- facerecognizer2.sh
+++ facerecognizer2.sh
@@ -7,5 +7,5 @@ then
 else
     echo "$SERVICE is not running and going to run now"
     cd /home/ubuntu/GitHub/face-recognition-python-opencv/
-    python /home/ubuntu/GitHub/face-recognition-python-opencv/facerecognizer2.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/ /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/ > /tmp/facerecognizer2.log &
+    python /home/ubuntu/GitHub/face-recognition-python-opencv/facerecognizer2.py /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/face/ /var/nodes/easyrtc/node_modules/easyrtc/server_example/uploads/ /var/nodes/easyrtc/node_modules/easyrtc/demos/latest/
 fi
