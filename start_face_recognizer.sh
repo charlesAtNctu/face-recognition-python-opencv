908c582742c46ee316b3da242c2acc30eac231f6
aime1 <charles.at.nctu@gmail.com>
2016-09-09 20:59:38 -0700

    have start shell script as well

---
+++ start_face_recognizer.sh
@@ -0,0 +1,7 @@
+/home/ubuntu/GitHub/face-recognition-python-opencv/facerecognizer2.sh
+
+#while true
+#do
+#    /home/ubuntu/GitHub/face-recognition-python-opencv/moveFileByName.sh
+#    sleep 10
+#done
