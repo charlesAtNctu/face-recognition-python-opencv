bd7a5db61be6321151254688acf33fc8288fd887
aime1 <charles.at.nctu@gmail.com>
2016-09-19 22:55:45 -0700

    add a python code to copy file for 1 min

---
+++ generateCopying.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os, sys
+import time
+from shutil import copyfile
+
+def main(argv):
+
+    copying_dir = argv[1];
+    src_file = argv[2];
+    tgt_file = argv[3];
+
+    count = 0
+    while (count < 60):
+        count = count + 1
+        print "" + count "\t: copying " + copying_dir+src_file + " " + copying_dir+tgt_file
+        copyfile(copying_dir+src_file, copying_dir+tgt_file)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main(sys.argv)
